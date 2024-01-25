c88c3a8e256e34da123b5112afee13142a6867f2
Rift343 <matthieu.conrath@gmail.com>
2024-01-25 21:47:19 +0100

    add some panic on the argument

--- Runner-Scheduler/src/main.rs
+++ Runner-Scheduler/src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    if (env::args().len())==1 {
+        panic!("Error: no semantic file receive")
+    };
+    if (env::args().len())>=3 {
+        panic!("Error: too many argument")
+    };
 }
