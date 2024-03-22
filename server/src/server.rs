be7dacc69f842b161705cfe76af691c4d41c281a
MelukHa <lucas.rebagliato@gmail.com>
2024-03-22 10:02:10 +0100

    ajout de commentaires plus pertinents dans le serveur et le client

--- server/src/server.rs
+++ server/src/server.rs
@@ -2,6 +2,7 @@ use std::io::{self, prelude::*, Error, ErrorKind};
 use std::net::{TcpListener, TcpStream};
 use engine::engine;
 
+// Function write_log to write all queries received in a log file 
 fn write_log(message: &str, log_file_path: &str) -> std::io::Result<()> {
     // Open the log file in append mode
     let mut file = std::fs::OpenOptions::new()
@@ -10,35 +11,38 @@ fn write_log(message: &str, log_file_path: &str) -> std::io::Result<()> {
         .append(true)
         .open(log_file_path)?;
 
-    // Write the log message to the file
+    // Write the received message to the file
     writeln!(file, "{}", message)?;
 
     Ok(())
 }
 
+// Function receive_response to receive the answer of the query (or technically any message) from a connected machine as a stream (here the server)
 fn receive_response(mut stream: &TcpStream) -> io::Result<String> {
-    // Read the response length from the client
+    // Read the response length from the server (maximum 4 bytes long)
     let mut len_bytes = [0; 4];
     stream.read_exact(&mut len_bytes)?;
 
-    // Convert the response length to u32
+    // Convert the answer length to u32
     let response_len = u32::from_be_bytes(len_bytes) as usize;
 
-    // Allocate a buffer for the response
+    // Allocate a buffer for the answer
     let mut response_buffer = vec![0; response_len];
 
-    // Use a loop to receive the response in chunks until all bytes are received
+    // We use a loop to receive the response in chunks until all bytes are received
     let mut bytes_received = 0;
     while bytes_received < response_len {
         let chunk = &mut response_buffer[bytes_received..];
         match stream.read(chunk) {
             Ok(0) => {
-                // End of stream reached prematurely
+                // End of stream reached prematurely (0 bytes received, that's an error)
                 return Err(Error::new(ErrorKind::UnexpectedEof, "End of stream reached prematurely"));
             }
             Ok(n) => {
+                // Here it went well, we had the bytes received to the total bytes received to double check we got the full response
                 bytes_received += n;
             }
+            // Otherwise we have an error because the stream was interrupted 
             Err(ref e) if e.kind() == ErrorKind::Interrupted => {}
             Err(e) => return Err(e),
         }
@@ -49,15 +53,16 @@ fn receive_response(mut stream: &TcpStream) -> io::Result<String> {
     Ok(response)
 }
 
+// Function send_query to send the query (or technically any message like an exit) to a connected machine as a stream (here the server)
 fn send_query(mut stream: &TcpStream, query: &str) -> io::Result<()> {
-    // Convert query to bytes
+    // We convert the query to bytes
     let query_bytes = query.as_bytes();
     let query_len = query_bytes.len() as u32; // Length of the query as u32
 
-    // Send the length of the query to the client
+    // Send the length of the query 
     stream.write_all(&query_len.to_be_bytes())?;
 
-    // Write the query to the client
+    // Send the query
     let mut bytes_sent = 0;
     while bytes_sent < query_bytes.len() {
         match stream.write(&query_bytes[bytes_sent..]) {
@@ -65,8 +70,8 @@ fn send_query(mut stream: &TcpStream, query: &str) -> io::Result<()> {
                 bytes_sent += n;
             }
             Ok(_) | Err(_) => {
-                // Handle write errors or cases where no bytes were sent
-                return Err(Error::new(ErrorKind::Other, "Error sending query to client"));
+                // Here we handle errors in the case where we had an explicit error or when we sent 0 bytes
+                return Err(Error::new(ErrorKind::Other, "Error sending query to server"));
             }
         }
     }
@@ -76,7 +81,7 @@ fn send_query(mut stream: &TcpStream, query: &str) -> io::Result<()> {
 
 fn handle_connection(mut stream: TcpStream) {
     loop {
-        // Receive the query from the client
+        // First we receive the query from the client (number of bytes + message)
         let query = match receive_response(&mut stream) {
             Ok(q) => q,
             Err(err) => {
@@ -85,6 +90,7 @@ fn handle_connection(mut stream: TcpStream) {
             }
         };
 
+        // We define the exit query and we verify if that's what the client sent
         let exit_string =String::from("exit") ;
         if query.trim() == exit_string {
             // Exit query, client has closed the connection
@@ -92,14 +98,15 @@ fn handle_connection(mut stream: TcpStream) {
             break;
         }
 
+        // print just to verify the server received the good message/query
         println!("Received query: {}", query);
 
-        // Log the query
+        // We log the query to the log file
         if let Err(err) = write_log(&query, "./log.txt") {
             println!("Error writing log: {}", err);
         }
 
-        // Processing the query
+        // We process the query by sending it to the engine and receiving its answer 
         let result = match engine(query.clone()) {
             Ok(response) => response,
             Err(err) => {
@@ -108,6 +115,7 @@ fn handle_connection(mut stream: TcpStream) {
             }
         };
 
+        // print just to verify we have the good answer before sending it to the client
         println!("Responding with: {}", result);
 
         // Send the response to the client
@@ -118,7 +126,7 @@ fn handle_connection(mut stream: TcpStream) {
     }
 }
 
-
+// Function create_listener that creates the connection between the client and the server 
 pub fn create_listener(addr: String) -> io::Result<()> {
     let listener = TcpListener::bind(addr)?;
     for stream in listener.incoming() {
