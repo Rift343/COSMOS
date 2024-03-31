5c905942ef4179003903c37c1de4e6489a2cc53f
CPavieR <mag@cleparo.fr>
2024-03-31 18:30:20 +0200

    mise en commentaires des tests

--- runner_scheduler/src/call_create.rs
+++ runner_scheduler/src/call_create.rs
@@ -9,26 +9,26 @@ use std::io::Read;
 use serde_json::Value;
 pub fn call_create(sub_requet:File)->Result<i8,Box<dyn Error>>
 {
-    let mut req = String::new();
-    sub_requet.read_to_string(&mut req).expect("Error: Unable to read syntaxic parsing file");
+    //let mut req = String::new();
+    //sub_requet.read_to_string(&mut req).expect("Error: Unable to read syntaxic parsing file");
 
     // Convert to a serde_json Value type
-    let parsing_value : Value = serde_json::from_str(&*req).expect("Error: Unable to turn JSON file into Value type");
+    //let parsing_value : Value = serde_json::from_str(&*req).expect("Error: Unable to turn JSON file into Value type");
     let mut r = 1;
-    let parse_json = parsing_value;
-    for (key,val) in parse_json{
-        let table_name = key;
-        let mut lst_attribut:Vec<String> = Vec::new();
-        for i in 0..val.len()
-        {
-            lst_attribut.push(parse_json["columns"][i]["column_name"].to_string());
-        }
-        let res = match relation_creater(&table_name,&lst_attribut) {
-            Ok(_) =>1 ,
-            Err(e) => return Err(Box::from(e)),
-        };
-        r = res;
-    }
+    //let parse_json = parsing_value;
+    //for (key,val) in parse_json{
+    //    let table_name = key;
+    //    let mut lst_attribut:Vec<String> = Vec::new();
+    //    for i in 0..val.len()
+    //    {
+    //        lst_attribut.push(parse_json["columns"][i]["column_name"].to_string());
+    //    }
+    //    let res = match relation_creater(&table_name,&lst_attribut) {
+    //        Ok(_) =>1 ,
+    //        Err(e) => return Err(Box::from(e)),
+    //    };
+    //    r = res;
+    //}
     
     Ok(r)
 }
